1f8dd341a4c08433062b378465ac0987d77dc79f
Cecilia <cecilia.sanchezlosada@gmail.com>
2024-04-23 11:31:00 +0200

    [Feature]: changing nav element class with js first approach

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
+window.addEventListener('load', function () {
+    let pageTitle = document.title;
+    let navEl = document.getElementById("nav-list").getElementsByTagName("li");
 
-
-console.log(document.title)
-
+    for (let item of navEl) {
+        if (pageTitle == item.textContent) {
+            let className = document.querySelector(`a#${item.textContent}.active`).classList;
+            console.log(className.toggle('on'));
+        }
+    }
+})

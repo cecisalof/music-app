3562f7396d0525f1977cf72fd301fee2e86183f8
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2024-06-17 11:58:27 +0200

    [Fix]: dist?

--- dist/index.68706f64.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll(".active").forEach(e=>{console.log(e.href),e.href===window.location.href&&(e.classList.add("on"),e.setAttribute("aria-current","page"))});
-//# sourceMappingURL=index.68706f64.js.map

d9dbbf44d85c399ce577939d1980354db76b0915
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2024-06-18 18:11:13 +0200

    Updates

---
+++ index.68706f64.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".active").forEach(e=>{console.log(e.href),e.href===window.location.href&&(e.classList.add("on"),e.setAttribute("aria-current","page"))});
+//# sourceMappingURL=index.68706f64.js.map
